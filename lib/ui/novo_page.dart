6472c2f5835692e962b338a0fd189ddb554dad34
luksk <lukskill@hotmail.com>
2021-06-14 21:25:47 -0300

    Interface de pagina de veiculos novos

--- lib/ui/novo_page.dart
+++ lib/ui/novo_page.dart
@@ -30,7 +30,7 @@ class _NovoPageState extends State<NovoPage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Imagem.criarImagemWeb("images/", w: 300),
-                          Text("Aguarde por favor..."),
+                          Text("Aguarde por favor!"),
                         ],
                       ),
                     );
